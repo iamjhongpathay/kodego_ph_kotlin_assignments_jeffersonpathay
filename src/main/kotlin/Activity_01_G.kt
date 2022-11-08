61d8a1a448c1e7b55119ca50fbb18b51a06d63cd
Jefferson Pathay <jeffersonpathay@gmail.com>
2022-11-08 21:25:00 +0800

    Activity_01_G.kt - 1st commit

---
+++ src/main/kotlin/Activity_01_G.kt
@@ -0,0 +1,39 @@
+/*
+Create an application that will accept 1 string.
+Your application will print “Palindrome” if the string is a palindrome.
+
+Scope :
+
+String
+Loops
+
+Ex :
+GIRAFARIG - Palindrome
+101 - Palindrome
+RACECAR - Palimdrome
+GATE - Not Palindrome
+ */
+fun main(){
+
+    //User Interface/Interaction
+    println("[      Welcome to KODEGO       ]" +
+            "\n[Mobile Application Development]")
+    println()
+
+    print("Enter a String: ")
+    var inputWord : String = readln().lowercase()
+
+    var reverseString : String = ""
+    var ctr : Int = inputWord.length - 1
+
+    //checking the string by reversed
+    while(ctr != -1){
+        reverseString += inputWord[ctr--]
+    }
+
+    //condition to know the inputWord and reverseString are equal to Palindrome or NOT
+    if(inputWord.uppercase() == reverseString.uppercase()){
+        println("${reverseString.uppercase()} - Palindrome")
+    }else
+        println("${reverseString.uppercase()} - Not Palindrome")
+}
